1620c7a29470f6a460e551f4988939f180d8067d
demilio24 <earias5@hawk.iit.edu>
2023-10-18 00:28:51 -0500

    fix clocks

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-let hrs = document.getElementById("hrs");
-let mins = document.getElementById("mins");
-let secs = document.getElementById("secs");
 
-let currentTime = new Date();
 
-hrs.innerHTML = currentTime.getHours();
